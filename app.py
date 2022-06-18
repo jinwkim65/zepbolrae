959be186723a42bf35749619f697bd47de026d97
Jinwoo Kim <jinwoo.kim@yale.edu>
2022-06-18 17:10:23 +0900

    changed requirements

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from audioop import add
-import calendar
 from cs50 import SQL
 from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-import json
-import pytz
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
